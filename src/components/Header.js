67bc6b83f40fb6fe1d0d541a3da396f5ad94023b
DanielDab <dab777799@gmail.com>
2022-09-29 21:40:54 -0300

    PreEntrega1-Borghese Corrección

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import imgHeader from '../assets/logo/maverick logo 5.png'
 
 const Header = () => {
     return (
             <header className="bg-light mt-5 py-5">
                 <div className="container d-flex justify-content-center">
-                    <img src="./assets/logo/maverick logo 5.png" alt="logo" width="300"/>
+                    <img src={imgHeader} alt="logo" width="300"/>
                 </div>
             </header>
     );
